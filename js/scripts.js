ab3aa933a01741a4fda38d428824dfead818a6ff
Randee Layosa and Slater Smith <slater@>
2019-01-22 16:41:19 -0800

    Add and rearrange arrays to allow for a sort function in console log.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
 $(document).ready(function() {
+
+
   $("#grocery-form").submit(function(event) {
-    var blanks = ["item1", "item2", "item3", "item4", "item5"];
+    var items = ["item1", "item2", "item3", "item4", "item5"];
+    var itemInput = [];
 
-
-    blanks.forEach(function(blank) {
-      var itemInput = $("input#" + blank).val();
-      $("." + blank).text(itemInput.toUpperCase());
-      // var alphabeticalInputs = capitalizedInputs.sort();
-      // $("." + blank).text(itemInput.sort());
-
-
-
-
+    items.forEach(function(item) {
+      itemInput.push($("input#" + item).val());
+      $("." + item).text(itemInput);
     });
 
+    itemInput.sort();
 
 
+    console.log(itemInput);
+
+
+
+        // var capItems = items.map(function(item) {
+        //   return item.toUppercase();
+        // });
+        // capItems.sort();
+        //
+        // capItems.forEach(function(item) {
+        //   $(".order").append("<li>" + items + "</li>");
+        // });
+
+
+
+      // var alphabeticalInputs = capitalizedInputs.sort();
+      // $("." + blank).text(itemInput.sort());
 
 
     $(".order").show();
